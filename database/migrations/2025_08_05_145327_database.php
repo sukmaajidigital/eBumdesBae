b65869f9627154ac90d1e861d292cd56f8c54c48
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-08-05 21:59:57 +0700

    create migration table and add role

---
+++ database/migrations/2025_08_05_145327_database.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kategoris', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')->constrained('kategoris')->onDelete('set null');
+            $table->decimal('price', 15, 2);
+            $table->longText('description');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('base_name');
+            $table->longText('description');
+            $table->string('email')->nullable();
+            $table->string('phone');
+            $table->longText('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+        Schema::dropIfExists('produks');
+        Schema::dropIfExists('kategoris');
+    }
+};
