13971990c730196e64308d1d18fb7ddf3ff536d1
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-08-13 08:33:31 +0700

    Update admin-header.blade.php

--- resources/views/components/admin-header.blade.php
+++ resources/views/components/admin-header.blade.php
@@ -3,13 +3,12 @@
         <div class="flex items-center justify-between h-16">
 
             <!-- Tombol untuk membuka Sidebar di Mobile -->
-            <a href="admin" class="text-xl font-medium text-gray-800">
+            <a href="{{ route('homepage') }}" class="text-xl font-medium text-gray-800">
                 Admin Panel
             </a>
 
             <!-- Bagian Kanan Header -->
-            <button @click="sidebarOpen = !sidebarOpen"
-                class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600 lg:hidden">
+            <button @click="sidebarOpen = !sidebarOpen" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600 lg:hidden">
                 <x-lucide-menu class="h-6 w-6" />
             </button>
         </div>
